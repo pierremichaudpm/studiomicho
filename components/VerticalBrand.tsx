2c38c26e425c603df7c166c84d995b9cedb08483
pierremichaudpm <pierre@example.com>
2026-01-24 14:37:33 -0500

    Fix vertical brand flash on mobile by setting default display none

--- components/VerticalBrand.tsx
+++ components/VerticalBrand.tsx
@@ -30,9 +30,12 @@ const VerticalBrand: React.FC = () => {
         STUDIO MICHO
       </a>
       <style jsx>{`
-        @media (max-width: 968px) {
+        .vertical-brand {
+          display: none;
+        }
+        @media (min-width: 969px) {
           .vertical-brand {
-            display: none !important;
+            display: block !important;
           }
         }
       `}</style>
